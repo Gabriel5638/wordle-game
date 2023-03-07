0e477ad46959353cee6e6b68593f63d182b926fb
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-03-07 16:47:32 +0000

    Add a red and green text color to rules

--- run.py
+++ run.py
@@ -25,3 +25,13 @@ while chances < max_chances and not true_check:
 
     if len(splitted_input) > 5 or len(splitted_input) < 5:
         print("Please enter a word with only 5 letters ")
+    else:
+        for i in range(len(splitted_input)):
+            if i != len(splitted_input) - 1:
+            if splitted_input[i] == splitted_random_word[i]:
+                print(colored(splitted_input[i], 'green'), end=" ")
+                true_check_counter += 1
+            elif splitted_input[i] in splitted_random_word:
+                print(colored(splitted_input[i], 'red'), end=" ")
+            else:
+                print(splitted_input[i], end=" ")
