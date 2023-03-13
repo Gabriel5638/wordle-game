b1b693c1aee511e0da720d147fbcf5587b6bf9e3
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-03-13 17:17:43 +0000

    Fix parsing and indent errors and get the game to work

--- run.py
+++ run.py
@@ -1,62 +1,64 @@
+"""
+Imports the color for the words.
+"""
+
 import random
 from termcolor import colored
-chances = 0
-max_chances = 5
-true_check = False
+
+CHANCES = 0
+MAX_CHANCES = 5
+TRUE_CHECK = False
 
 words = [
-            "cat",
-            "dog",
-            "rat",
-            "hat",
-            "mat",
-    ]
+    "tiger",
+    "beach",
+    "rat",
+    "hat",
+    "mat",
+]
 
 random_words = random.choice(words)
-splitted_random_word = ([*random_words])
+splitted_random_word = [*random_words]
 
-   print("/n******Title of game******\n")
+print("\n******Title of game******\n")
 
-while chances < max_chances and not true_check:
+while CHANCES < MAX_CHANCES and not TRUE_CHECK:
+    user_input = input("Guess: ")
+    splitted_input = [*user_input]
+    TRUE_CHECK_COUNTER = 0
 
-        user_input = input("Guess: ")
-        splitted_input = ([*user_input])
-        true_check_counter = 0
-         if len(splitted_input) > 5 or len(splitted_input) < 5:
-     print("Please enter a word with only 5 letters ")
-
-        else:
-
-       for i in range(len(splitted_input)):
-           if i != len(splitted_input) - 1:
-           if splitted_input[i] == splitted_random_word[i]:
-      # Print the word in green if it is correct
-     print(colored(splitted_input[i], 'green'), end=" ")
-    true_check_counter += 1
-        elif splitted_input[i] in splitted_random_word:
-    # Print the word in red if it is incorrect
-     print(colored(splitted_input[i], 'red'), end=" ")
-         else:
-     # Print the word without color if it is not present in the generated word
-     print(splitted_input[i], end=" ")
-         else:
-          if splitted_input[i] == splitted_random_word[i]:
-     print(colored(splitted_input[i], 'green'))
-        true_check_counter += 1
-         elif splitted_input[i] in splitted_random_word:
-        print(colored(splitted_input[i], 'red'))
-          else:
-         print(splitted_input[i])
-
-          if true_check_counter < 4:
-    pass
+    if len(splitted_input) > 5 or len(splitted_input) < 5:
+        print("Please enter a word with only 5 letters ")
     else:
-    true_check = True 
-            
-      chances += 1 
-    
+        for i, word in enumerate(splitted_input):
+            if i != len(splitted_input) - 1:
+                if word == splitted_random_word[i]:
+                    # Print the word in green if it is correct
+                    print(colored(word, 'green'), end=" ")
+                    TRUE_CHECK_COUNTER += 1
+                elif word in splitted_random_word:
+                    # Print the word in red if it is incorrect
+                    print(colored(word, 'red'), end=" ")
+                else:
+                    # Print the word without color if it is not present
+                    print(word, end=" ")
+            else:
+                if word == splitted_random_word[i]:
+                    print(colored(word, 'green'))
+                    TRUE_CHECK_COUNTER += 1
+                elif word in splitted_random_word:
+                    print(colored(word, 'red'))
+                else:
+                    print(word)
 
-    if true_check:
-         print("You guessed the word!")
-    else: 
-          print("You couldn't guess the correct word! It was", random_words)
+        if TRUE_CHECK_COUNTER < 4:
+            pass
+        else:
+            TRUE_CHECK = True
+
+        CHANCES += 1
+
+if TRUE_CHECK:
+    print("You guessed the word!")
+else:
+    print("You couldn't guess the correct word! It was", random_words)
